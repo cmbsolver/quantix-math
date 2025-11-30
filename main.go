95c563dfb41b59645cecf3a33dad5dfed23762bb
cmbsolver <cmbsolver@gmail.com>
2025-11-30 01:14:42 -0600

    Set up UI rendering with Fiber templates: add layouts, views, and routes for Home and About pages. Configure `html/v2` template engine.

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"db"
+	"quantix-math/routes" // Import the new routes package
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
 	// init database
 	db.InitDatabase()
 
-	app := fiber.New()
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".tmpl")
 
-	// Static files
-	app.Static("/", "./quantix-math-ui/dist")
+	// Pass the engine to the Fiber app
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+	})
+
+	// Setup Routes
+	routes.SetupUIRoutes(app)
 
 	app.Listen(":3000")
 }
